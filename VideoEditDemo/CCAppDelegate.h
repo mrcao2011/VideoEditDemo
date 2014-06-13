729792c575f160fd7bc7b51fce187ff69cc94e5a
caocao <mrcao@mrcaodeMac-mini.local>
2014-06-13 09:18:23 +0800

    Initial Commit

---
+++ VideoEditDemo/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  VideoEditDemo
+//
+//  Created by mr.cao on 14-6-13.
+//  Copyright (c) 2014年 mrcao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoEditDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoEditDemo
+//
+//  Created by mr.cao on 14-6-13.
+//  Copyright (c) 2014年 mrcao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
